cedb75d91ab7cf82751fea1c92febc72191b8c9f
Maxim Dukhanov <m.dukhanov@gmail.com>
2017-06-10 17:42:51 +0300

    Updated readme

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # cordova-simple-timer
 
 Simple timer plugin which uses Android Broadcast action: ACTION_TIME_TICK
+
+## Installation
+```
+$ cordova plugin add https://www.github.com/dukhanov/cordova-simple-timer
+```
+### Android
+
+cordova build android
+
+## Plugin API
+
+It has been currently stripped to the minimum needed from a Javascript app.
+
+The following functions are available:
+
+* `SimpleTimer.start(listener, error)`. Start timer with 1 minut interval
+  * listener: called on timer tick
+  * error: called on function error
+* `SimpleTimer.stop(success, error)`. Stop timer
+  * success: called on function success
+  * error: called on function error
+
+## Sample usage code
+```Javascript
+function onTimerTick() {
+    console.log('timer tick');
+}
+
+function errorStart(message) {
+    console.log('timer start failed: ' + message);
+}
+
+function onStopped() {
+    console.log('timer is stopped');
+}
+
+// Start
+GpsStatus.start(onTimerTick, errorStart);
+
+// Stop
+GpsStatus.stop(onStopped)
+```
--- www/SimpleTimer.js
+++ www/SimpleTimer.js
@@ -1,26 +1,25 @@
 var exec = require('cordova/exec');
 
-function SimpleTimer() {
-}
-
-SimpleTimer.prototype.start = function (callback, error) {
-    if (this.isStarted) {
-        console.warn('SimpleTimer is already started');
+var SimpleTimer = {
+    isStarted: false,
+    start: function (callback, error) {
+        if (this.isStarted) {
+            console.warn('SimpleTimer is already started');
+        }
+        this.isStarted = true;
+        callback = callback || function() {};
+        error = error || function() {};
+        exec(callback, error, 'SimpleTimer', 'start', []);
+    },
+    stop: function (success, error) {
+        if (!this.isStarted) {
+            console.warn('SimpleTimer is already stopped');
+        }
+        this.isStarted = false;
+        success = success || function() {};
+        error = error || function() {};
+        exec(success, error, 'SimpleTimer', 'stop', []);
     }
-    this.isStarted = true;
-    callback = callback || function() {};
-    error = error || function() {};
-    exec(callback, error, 'SimpleTimer', 'start', []);
-};
-
-SimpleTimer.prototype.stop = function (success, error) {
-    if (!this.isStarted) {
-        console.warn('SimpleTimer is already stopped');
-    }
-    this.isStarted = false;
-    success = success || function() {};
-    error = error || function() {};
-    exec(success, error, 'SimpleTimer', 'stop', []);
 };
 
 module.exports = SimpleTimer;
